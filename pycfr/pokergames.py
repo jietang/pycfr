9b2216aa6b8f48769fbfa5534e3742dae9aa55d7
Jie Tang <jietang@openai.com>
2018-06-05 16:39:20 -0700

    Allow passing infoset format into rules

--- pycfr/pokergames.py
+++ pycfr/pokergames.py
@@ -1,16 +1,22 @@
 from .pokertrees import *
 from .card import Card
 
+def leduc_format(player, holecards, board, bet_history):
+    cards = holecards[0].RANK_TO_STRING[holecards[0].rank]
+    if len(board) > 0:
+        cards += board[0].RANK_TO_STRING[board[0].rank]
+    return "{0}:{1}:".format(cards, bet_history)
+
 def kuhn_eval(hc, board):
     return hc[0].rank
 
-def half_street_kuhn_rules():
+def half_street_kuhn_rules(infoset_format=leduc_format):
     players = 2
     deck = [Card(14,1),Card(13,1),Card(12,1)]
     ante = 1
     blinds = None
     rounds = [RoundInfo(holecards=1,boardcards=0,betsize=1,maxbets=[1,0])]
-    return GameRules(players, deck, rounds, ante, blinds, handeval=kuhn_eval, infoset_format=leduc_format)
+    return GameRules(players, deck, rounds, ante, blinds, handeval=kuhn_eval, infoset_format=infoset_format)
 
 def half_street_kuhn_gametree():
     rules = half_street_kuhn_rules()
@@ -24,13 +30,13 @@ def half_street_kuhn_publictree():
     tree.build()
     return tree
 
-def kuhn_rules():
+def kuhn_rules(infoset_format=leduc_format):
     players = 2
     deck = [Card(14,1),Card(13,1),Card(12,1)]
     ante = 1
     blinds = None
     rounds = [RoundInfo(holecards=1,boardcards=0,betsize=1,maxbets=[1,1])]
-    return GameRules(players, deck, rounds, ante, blinds, handeval=kuhn_eval, infoset_format=leduc_format)
+    return GameRules(players, deck, rounds, ante, blinds, handeval=kuhn_eval, infoset_format=infoset_format)
 
 def kuhn_gametree():
     rules = kuhn_rules()
@@ -44,25 +50,19 @@ def kuhn_publictree():
     tree.build()
     return tree
 
-def leduc_format(player, holecards, board, bet_history):
-    cards = holecards[0].RANK_TO_STRING[holecards[0].rank]
-    if len(board) > 0:
-        cards += board[0].RANK_TO_STRING[board[0].rank]
-    return "{0}:{1}:".format(cards, bet_history)
-
 def leduc_eval(hc,board):
     hand = hc + board
     if hand[0].rank == hand[1].rank:
         return 15*14+hand[0].rank
     return max(hand[0].rank, hand[1].rank) * 14 + min(hand[0].rank, hand[1].rank)
 
-def leduc_rules():
+def leduc_rules(infoset_format=leduc_format):
     players = 2
     deck = [Card(13,1),Card(13,2),Card(12,1),Card(12,2),Card(11,1),Card(11,2)]
     ante = 1
     blinds = None
     rounds = [RoundInfo(holecards=1,boardcards=0,betsize=2,maxbets=[2,2]),RoundInfo(holecards=0,boardcards=1,betsize=4,maxbets=[2,2])]
-    return GameRules(players, deck, rounds, ante, blinds, handeval=leduc_eval, infoset_format=leduc_format)
+    return GameRules(players, deck, rounds, ante, blinds, handeval=leduc_eval, infoset_format=infoset_format)
 
 def leduc_gametree():
     rules = leduc_rules()
@@ -103,7 +103,7 @@ def royal_eval(hc,board):
         return 100+hand[0].rank
     return hand[0].rank
 
-def royal_rules():
+def royal_rules(infoset_format=royal_format):
     players = 2
     deck = [Card(14,1),Card(14,2),Card(13,1),Card(13,2),Card(12,1),Card(12,2),Card(11,1),Card(11,2)]
     ante = 1
@@ -112,7 +112,7 @@ def royal_rules():
     flop = RoundInfo(holecards=0,boardcards=1,betsize=4,maxbets=[2,2])
     turn = RoundInfo(holecards=0,boardcards=1,betsize=4,maxbets=[2,2])
     rounds = [preflop,flop,turn]
-    return GameRules(players, deck, rounds, ante, blinds, handeval=royal_eval, infoset_format=royal_format)
+    return GameRules(players, deck, rounds, ante, blinds, handeval=royal_eval, infoset_format=infoset_format)
 
 def royal_gametree():
     rules = royal_rules()
